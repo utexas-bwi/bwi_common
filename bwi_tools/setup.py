1c325b2534f640a88f4865271ee275f196d54a2a
piyushk <piyushk@gmail.com>
2013-12-21 17:24:43 +0530

    fixed indentation in setup.py

--- bwi_tools/setup.py
+++ bwi_tools/setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-        packages=['bwi_tools'],
-        package_dir={'': 'src'},
+    packages=['bwi_tools'],
+    package_dir={'': 'src'},
 )
 
 setup(**d)
