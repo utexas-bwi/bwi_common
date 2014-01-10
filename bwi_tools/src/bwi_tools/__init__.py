e4c970a594dfb009c345fe6c668ae4c466ae7e5f
Piyush Khandelwal <piyushk@gmail.com>
2014-01-10 11:48:25 -0600

    added roslaunch helper to bwi_tools

--- bwi_tools/src/bwi_tools/__init__.py
+++ bwi_tools/src/bwi_tools/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
 from .mapper import loadMapFromFile, saveMapToFile
-from .wall_rate import WallRate
+from .roslaunch import start_roslaunch_process, stop_roslaunch_process
 from .timer import Timer
+from .wall_rate import WallRate
---
+++ bwi_tools/src/bwi_tools/roslaunch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import subprocess
+
+def start_roslaunch_process(package, binary, args=None, log=None):
+    if args == None:
+        args = {}
+    command_args = ['roslaunch', package, binary]
+    command_args.extend([key + ':=' + value for key, value in args.iteritems()])
+    command_args.append('--screen')
+    print "Running command: " + ' '.join(command_args)
+    return (subprocess.Popen(command_args, stdout=log, stderr=subprocess.STDOUT)
+            if log != None else subprocess.Popen(command_args))
+
+def stop_roslaunch_process(process):
+    process.terminate()
