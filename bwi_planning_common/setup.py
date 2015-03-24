d71b1d57deb4ca11c4561276d698d73538958322
Piyush Khandelwal <piyushk@gmail.com>
2015-03-23 20:00:06 -0500

    removed accidental global script installation. closes #18.

--- bwi_planning_common/setup.py
+++ bwi_planning_common/setup.py
@@ -6,7 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['bwi_planning_common'],
     package_dir={'': 'src'},
-    scripts={'scripts/logical_marker'},
 )
 
 setup(**d)
