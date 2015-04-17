d1cfc1c8c4b59a1ae09af270d1cb9458c7c21dcc
Piyush Khandelwal <piyushk@gmail.com>
2015-04-17 17:45:31 -0500

    update roslaunch launcher to allow directly running a launch file using an absolute path.

--- bwi_tools/src/bwi_tools/roslaunch.py
+++ bwi_tools/src/bwi_tools/roslaunch.py
@@ -5,7 +5,10 @@ import subprocess
 def start_roslaunch_process(package, binary, args=None, log=None):
     if args == None:
         args = {}
-    command_args = ['roslaunch', package, binary]
+    if package is not None:
+        command_args = ['roslaunch', package, binary]
+    else:
+        command_args = ['roslaunch', binary]
     command_args.extend([key + ':=' + value for key, value in args.iteritems()])
     command_args.append('--screen')
     print "Running command: " + ' '.join(command_args)
