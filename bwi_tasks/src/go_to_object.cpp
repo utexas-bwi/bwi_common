faa9730cef82df51e5afb6a7543b358cfe895dce
FernandezR <rfernandez@utexas.edu>
2016-06-21 10:11:31 -0500

    Created go_to_object bwi_tasks, which allows the robot to go to objects annotated on the map.

---
+++ bwi_tasks/src/go_to_object.cpp
@@ -0,0 +1,64 @@
+
+#include "bwi_kr_execution/ExecutePlanAction.h"
+
+#include <actionlib/client/simple_action_client.h>
+
+#include <ros/ros.h>
+
+typedef actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> Client;
+
+using namespace std;
+
+int main(int argc, char**argv) {
+  ros::init(argc, argv, "go_to_object");
+  ros::NodeHandle n;
+  
+  ros::NodeHandle privateNode("~");
+  string object;
+  privateNode.param<string>("object",object,"o3_434_soccer");
+  
+  ROS_INFO_STREAM("going to " << object);
+  
+  Client client("action_executor/execute_plan", true);
+  client.waitForServer();
+  
+  bwi_kr_execution::ExecutePlanGoal goal;
+  
+  bwi_kr_execution::AspRule rule;
+  bwi_kr_execution::AspFluent fluent;
+  fluent.name = "not facing";
+  
+  fluent.variables.push_back(object);
+ 
+  rule.body.push_back(fluent);
+  goal.aspGoal.push_back(rule);
+  
+  ROS_INFO("sending goal");
+  client.sendGoal(goal);
+  
+  ros::Rate wait_rate(10);
+  while(ros::ok() && !client.getState().isDone())
+    wait_rate.sleep();
+    
+  if (!client.getState().isDone()) {
+    ROS_INFO("Canceling goal");
+    client.cancelGoal();
+    //and wait for canceling confirmation
+    for(int i = 0; !client.getState().isDone() && i<50; ++i)
+      wait_rate.sleep();
+  }
+  if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
+    ROS_INFO("Aborted");
+  }
+  else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+    ROS_INFO("Preempted");
+  }
+  
+  else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+    ROS_INFO("Succeeded!");
+  }
+  else
+     ROS_INFO("Terminated");
+    
+  return 0;
+}
