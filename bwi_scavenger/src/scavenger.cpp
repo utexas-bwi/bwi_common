860b6831ea09eefac31eec5533e4811f45e183b2
Shiqi Zhang <shiqi.zhang6@gmail.com>
2016-06-24 12:49:37 -0500

    minor

--- bwi_scavenger/src/scavenger.cpp
+++ bwi_scavenger/src/scavenger.cpp
@@ -40,17 +40,22 @@ bool callback_srv_scav(bwi_msgs::ScavHunt::Request &req,
                        bwi_msgs::ScavHunt::Response &res) {
  
     ROS_INFO_STREAM("callback_srv_scav is called (0 pause, 1 resume): " << curr_task->status);
+
     if ((int) req.type == bwi_msgs::ScavHuntRequest::SCAV_PAUSE) {
         curr_task->status = TODO; 
         curr_task->task->stopEarly(); 
         task_manager->paused = true; 
 
-        actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> client("/action_executor/execute_plan", true);
+        actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> 
+            client("/action_executor/execute_plan", true);
+
         ros::spinOnce(); 
         ros::spinOnce(); 
         client.cancelAllGoals (); 
 
-        ros::Publisher pub = nh->advertise<actionlib_msgs::GoalID>("/move_base/cancel", 10);
+        ros::Publisher pub = nh->advertise<actionlib_msgs::GoalID>
+            ("/move_base/cancel", 10);
+
         actionlib_msgs::GoalID msg; 
         msg.id = ""; 
         // msg.stamp = ros::Time::now();
@@ -59,7 +64,6 @@ bool callback_srv_scav(bwi_msgs::ScavHunt::Request &req,
         pub.publish(msg);
         ros::spinOnce(); 
         pub.publish(msg);
-        ros::spinOnce(); 
 
     } else if ((int) req.type == bwi_msgs::ScavHuntRequest::SCAV_RESUME) {
         task_manager->paused  = false; 
