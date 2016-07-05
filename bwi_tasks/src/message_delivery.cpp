1d88498790e350b3de71816b3f66cd6436389599
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-07-05 16:55:22 -0500

    Added files

---
+++ bwi_tasks/src/message_delivery.cpp
@@ -0,0 +1,80 @@
+#include "bwi_kr_execution/ExecutePlanAction.h"
+
+#include <actionlib/client/simple_action_client.h>
+
+#include <sound_play/sound_play.h>
+
+#include <ros/ros.h>
+
+typedef actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> Client;
+
+std::string message;
+std::string recipient;
+
+void goToLocation() {  
+	Client client("action_executor/execute_plan", true);
+	client.waitForServer();
+	  
+	bwi_kr_execution::ExecutePlanGoal goal;
+	  
+	bwi_kr_execution::AspRule rule;
+	bwi_kr_execution::AspFluent fluent;
+	fluent.name = "not at";
+	  
+	fluent.variables.push_back(recipient);
+	 
+	rule.body.push_back(fluent);
+	goal.aspGoal.push_back(rule);
+	  
+	ROS_INFO("sending goal");
+	client.sendGoal(goal);
+	  
+	ros::Rate wait_rate(10);
+	while(ros::ok() && !client.getState().isDone())
+	  wait_rate.sleep();
+	    
+	if (!client.getState().isDone()) {
+		ROS_INFO("Canceling goal");
+		client.cancelGoal();
+	    //and wait for canceling confirmation
+		for(int i = 0; !client.getState().isDone() && i < 50; ++i)
+			wait_rate.sleep();
+	}
+	if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
+		ROS_INFO("Aborted");
+	}
+	else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+		ROS_INFO("Preempted");
+	}
+	else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+		ROS_INFO("Succeeded!");
+	}
+	else
+		ROS_INFO("Terminated");
+}
+
+void deliverMessage() {
+    ros::NodeHandle n;
+    ros::Publisher message_pub = n.advertise<sound_play::SoundRequest>("robotsound", 1);
+    sound_play::SoundRequest sound_req;
+    sound_req.sound = sound_play::SoundRequest::SAY;
+    sound_req.command = sound_play::SoundRequest::PLAY_ONCE;
+    std::stringstream ss;
+    ss << "Message for " << recipient << ". " << message;
+    sound_req.arg = ss.str();
+    
+    message_pub.publish(sound_req);
+}
+
+int main(int argc, char** argv) {
+	//initialize the node
+	ros::init(argc, argv, "message_delivery");
+
+	ros::NodeHandle privateNode("~");
+
+	privateNode.param<std::string>("recipient",recipient, /*default*/ "l3_414b"); 
+	privateNode.param<std::string>("message",message,"Empty");
+
+	goToLocation();
+	deliverMessage();
+}
