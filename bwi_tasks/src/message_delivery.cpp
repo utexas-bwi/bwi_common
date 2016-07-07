a05792f71fb67c5acd152220da26bacf4537cb69
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-07-07 15:04:01 -0500

    Fixed sound_play issues

--- bwi_tasks/src/message_delivery.cpp
+++ bwi_tasks/src/message_delivery.cpp
@@ -1,68 +1,85 @@
+
+#include "bwi_kr_execution/ExecutePlanAction.h"
+
+#include <actionlib/client/simple_action_client.h>
+
 #include <sound_play/sound_play.h>
-#include <unistd.h>
+
+#include <ros/ros.h>
+
+#include <string>
+
+typedef actionlib::SimpleActionClient<bwi_kr_execution::ExecutePlanAction> Client;
+
+std::string message;
+std::string recipient;
+
+void goToLocation() {  
+	Client client("action_executor/execute_plan", true);
+	client.waitForServer();
+	  
+	bwi_kr_execution::ExecutePlanGoal goal;
+	  
+	bwi_kr_execution::AspRule rule;
+	bwi_kr_execution::AspFluent fluent;
+	fluent.name = "not at";
+	  
+	fluent.variables.push_back(recipient);
+	 
+	rule.body.push_back(fluent);
+	goal.aspGoal.push_back(rule);
+	  
+	ROS_INFO("sending goal");
+	client.sendGoal(goal);
+	  
+	ros::Rate wait_rate(10);
+	while(ros::ok() && !client.getState().isDone())
+	  wait_rate.sleep();
+	    
+	if (!client.getState().isDone()) {
+		ROS_INFO("Canceling goal");
+		client.cancelGoal();
+	    //and wait for canceling confirmation
+		for(int i = 0; !client.getState().isDone() && i < 50; ++i)
+			wait_rate.sleep();
+	}
+	if (client.getState() == actionlib::SimpleClientGoalState::ABORTED) {
+		ROS_INFO("Aborted");
+	}
+	else if (client.getState() == actionlib::SimpleClientGoalState::PREEMPTED) {
+		ROS_INFO("Preempted");
+	}
+	else if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+		ROS_INFO("Succeeded!");
+	}
+	else
+		ROS_INFO("Terminated");
+}
 
 void sleepok(int t, ros::NodeHandle &nh)
 {
-	   if (nh.ok())
-                 sleep(t);
- }
- 
- int main(int argc, char **argv)
- {
-   ros::init(argc, argv, "sound_play_test");
- 
-   ros::NodeHandle nh;
-   sound_play::SoundClient sc;
- 
-   sleepok(1, nh);
-   
-   while(nh.ok())
-   {
-     sc.say("Hello world!");
-     sleepok(2, nh);
- 
-     const char *str1 = "I am annoying.";
-     sc.repeat(str1);
-     sleepok(4, nh);
-     sc.stopSaying(str1);
- 
-     sc.playWave("/usr/share/xemacs21/xemacs-packages/etc/sounds/boing.wav");
-     sleepok(2, nh);
- 
-     const char *str2 = "/usr/share/xemacs21/xemacs-packages/etc/sounds/piano-beep.wav";
-     sc.startWave(str2);
-     sleepok(4, nh);
-     sc.stopWave(str2);
- 
-     sc.play(sound_play::SoundRequest::NEEDS_UNPLUGGING);
-     sleepok(2, nh);
- 
-     sc.start(sound_play::SoundRequest::BACKINGUP);
-     sleepok(4, nh);
-     sc.stop(sound_play::SoundRequest::BACKINGUP);
+	if (nh.ok())
+		sleep(t);
+}
 
-                 sleepok(2, nh);
-                 sound_play::Sound s1 = sc.waveSound("/usr/share/xemacs21/xemacs-packages/etc/sounds/boing.wav");
-                 s1.repeat();
-                 sleepok(1, nh);
-                 s1.stop();
-                 
-                 sleepok(2, nh);
-                 sound_play::Sound s2 = sc.voiceSound("This is a really long sentence that will get cut off.");
-                 s2.play();
-                 sleepok(1, nh);
-                 s2.stop();
- 
-                 sleepok(2, nh);
-                 sound_play::Sound s3 = sc.builtinSound(sound_play::SoundRequest::NEEDS_UNPLUGGING_BADLY);
-                 s3.play();
-                 sleepok(1, nh);
-                 s3.stop();
- 
-                 sleepok(2, nh);
-                 sound_play::Sound s4 = sc.waveSoundFromPkg("sound_play", "sounds/BACKINGUP.ogg");
-                 s4.play();
-                 sleepok(1, nh);
-                 s4.stop();
-   }
- }
+void deliverMessage(sound_play::SoundClient &sc, ros::NodeHandle &nh) {
+	sleepok(2, nh);
+	sc.say("Message for " + recipient + ". " + message);
+	sleepok(2, nh);
+}
+
+int main(int argc, char** argv) {
+	//initialize the node
+	ros::init(argc, argv, "message_delivery");
+
+	ros::NodeHandle privateNode("~");
+
+	privateNode.param<std::string>("recipient",recipient, /*default*/ "l3_414b"); 
+	privateNode.param<std::string>("message",message,"Empty");
+	
+	ros::NodeHandle nh;
+    sound_play::SoundClient sc;
+
+	//goToLocation();
+	deliverMessage(sc, nh);
+}
