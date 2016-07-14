7955b9ab6f0850fcfc5001da530e5a71a3ac5607
Benjamin Singer <benjamin.z.singer@gmail.com>
2016-07-14 17:10:50 -0500

    Completed client, ready to merge

--- bwi_services/src/deliver_message_client.cpp
+++ bwi_services/src/deliver_message_client.cpp
@@ -4,23 +4,24 @@
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "deliver_message_client");
+	ros::init(argc, argv, "deliver_message_client");
+	if (argc != 2) {
+		ROS_INFO("usage: deliver_message_client location message");
+	}
 
-  ros::NodeHandle n;
-  ros::ServiceClient client = n.serviceClient<bwi_services::DeliverMessage>("deliver_message");
-  bwi_services::DeliverMessage srv;
-  srv.request.location = argv[1];
-  srv.request.message = argv[2];
+ 	ros::NodeHandle n;
+	ros::ServiceClient client = n.serviceClient<bwi_services::DeliverMessage>("deliver_message");
+	bwi_services::DeliverMessage srv;
+	srv.request.location = argv[1];
+	srv.request.message = argv[2];
 
-  if (client.call(srv))
-  {
-    ROS_INFO("Travelling to %s to deliver message: %s", argv[1], argv[2]);
-  }
-  else
-  {
-    ROS_ERROR("Failed to call service");
-    return 1;
-  }
+	if (client.call(srv)) {
+		ROS_INFO("Travelling to %s to deliver message: %s", argv[1], argv[2]);
+  	}
+  	else {
+    		ROS_ERROR("Failed to call service");
+    		return 1;
+  	}
 
-  return 0;
+  	return 0;
 }
