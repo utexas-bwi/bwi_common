f5f58f66b001095615c8af8b986a7945fe8778d8
Pato <plankenau@gmail.com>
2016-07-29 09:53:11 -0500

    removing old script files

--- bwi_virtour/setupPorts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ssh -L 9090:localhost:9090 http://nixons-head.csres.utexas.edu # rosbridge
-ssh -L 8080:localhost:8080 http://nixons-head.csres.utexas.edu # video stream
-ssh -L 8000:localhost:8000 http://nixons-head.csres.utexas.edu # image server
