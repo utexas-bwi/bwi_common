365c9e4bf94b1343fa4d00d3dd0980f58c0bdc68
Pato <plankenau@gmail.com>
2016-07-29 10:02:31 -0500

    got rid of old script

--- bwi_virtour/broadcastTour.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-hostname=$(hostname)
-link="http://nixons-head.csres.utexas.edu/tours"
-to="your.email@gmail.com"
-
-echo -e "Tour started on $hostname\nCheck it out at: $link" | mail -s "[bwi_virtour] Tour started on $hostname" "$to"
