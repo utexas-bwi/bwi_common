dd32273f3d5e57ab128efe35b16c12699bef4c41
Nicu Stiurca <nicu@cogitai.com>
2016-08-08 15:51:24 -0500

    Fix missing includes in OpenCV 3

--- bwi_mapper/src/libbwi_mapper/voronoi_approximator.cpp
+++ bwi_mapper/src/libbwi_mapper/voronoi_approximator.cpp
@@ -43,6 +43,12 @@
 #include <bwi_mapper/map_utils.h>
 #include <bwi_mapper/point_utils.h>
 
+#include <opencv2/opencv_modules.hpp>
+#ifdef HAVE_OPENCV_IMGPROC
+// this is OpenCV 3 and we need extra includes
+#include <opencv2/imgproc/imgproc.hpp>
+#endif
+
 namespace bwi_mapper {
 
   /**
