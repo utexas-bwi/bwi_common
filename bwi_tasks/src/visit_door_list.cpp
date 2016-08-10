f87da5100a14c1a153c1d742b67748aee004a5a0
nj1407 <nj1407@gmail.com>
2016-08-10 10:46:42 -0500

    bwi_common

--- bwi_tasks/src/visit_door_list.cpp
+++ bwi_tasks/src/visit_door_list.cpp
@@ -24,12 +24,22 @@ int main(int argc, char**argv) {
   
   std::vector<string> doors;
 
-  doors.push_back("d3_414b1");
-  doors.push_back("d3_414b2");
-  doors.push_back("d3_414a1");
-  doors.push_back("d3_414a2");
+  doors.push_back("d4_828");
+  doors.push_back("d4_710a2");
+  doors.push_back("d4_710a1");
+  doors.push_back("d4_600");
+  doors.push_back("d4_102");
+  doors.push_back("d4_500");
+  doors.push_back("d4_414a2");
+  doors.push_back("d4_400");
+  doors.push_back("d4_414a1");
+  doors.push_back("d4_414b3");
+  doors.push_back("d4_414b2");
+  doors.push_back("d4_414b1");
+  doors.push_back("d4_516a");
+  doors.push_back("d4_516b");
+  doors.push_back("d4_440");
 
-  doors.push_back("d3_418");
   int current_door = 0;
 
   Client client("/action_executor/execute_plan", true);
