a0a18f890832b99a31042dfcc35d895a973f4c59
Nick Walker <nick@nickwalker.us>
2018-03-07 11:15:57 -0600

    Add ArmPositionDB class
    Promoted from segbot_arm_manipulation
    Add proper namespacing
    Tweaks for consistency, safety
    Add missing agile_grasp dependency
    Add a note on the purpose of the Arm class stub

--- bwi_manipulation/include/bwi_manipulation/arm.h
+++ bwi_manipulation/include/bwi_manipulation/arm.h
@@ -2,7 +2,9 @@
 #define BWI_MANIPULATION_ARM_H
 namespace bwi_manipulation {
     class Arm {
-
+    //TODO: Figure out this abstraction
+        // This is a stub until we get a second arm and actually have to figure out a reasonable
+        // common interface.
     };
 }
 
