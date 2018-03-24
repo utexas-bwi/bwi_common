dc158816d19fdb66641d80c20fbcd8c95b1a1ee5
JGOOSH <jgoo@utexas.edu>
2018-03-24 14:48:41 -0500

    corrected the angel indexing

--- bwi_wandering/src/safety.cpp
+++ bwi_wandering/src/safety.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
     ros::NodeHandle nh_;
     tf::TransformListener tf_l;
 
-    ros::Subscriber laser_sub = nh_.subscribe("/laserscan", 1000, laser_cb);
+    ros::Subscriber laser_sub = nh_.subscribe("/scan_filtered", 1000, laser_cb);
     ros::Publisher twist_pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
     ros::Publisher safe_pub = nh_.advertise<std_msgs::Bool>("/cmd_safe",1000);    
 
@@ -39,33 +39,44 @@ int main(int argc, char **argv) {
     // assuming that this value is not arbritary, checking if it is okay to proceed
     while(ros::ok()){
       ros::spinOnce();
-      try{
-        ROS_INFO("Transform twist angular to laser's frame");
-        geometry_msgs::Vector3Stamped stamped_in;
-        stamped_in.vector = twist_msg.angular;
-        stamped_in.header.frame_id = "/base_link";
-        geometry_msgs::Vector3Stamped stamped_out;
-        // tf_l.waitForTransform(laser_msg.header.frame_id, "/base_link", ros::Time(0), ros::Duration(2));
-        tf_l.transformVector(laser_msg.header.frame_id, stamped_in , stamped_out);
-      }
-      catch (tf::TransformException ex) {
-        ROS_INFO ("%s", ex.what());
-      }
+      // try{
+      //   ROS_INFO("Transform twist angular to laser's frame");
+      //   geometry_msgs::Vector3Stamped stamped_in;
+      //   stamped_in.vector = twist_msg.angular;
+      //   stamped_in.header.frame_id = "/base_link";
+      //   geometry_msgs::Vector3Stamped stamped_out;
+      //   // tf_l.waitForTransform(laser_msg.header.frame_id, "/base_link", ros::Time(0), ros::Duration(2));
+      //   tf_l.transformVector(laser_msg.header.frame_id, stamped_in , stamped_out);
+      // }
+      // catch (tf::TransformException ex) {
+      //   ROS_INFO ("%s", ex.what());
+      // }
 
-      float dist = laser_msg.ranges[twist_msg.angular.z];
-      ROS_INFO("Distance from laser is %f", dist);
-
-      if (dist > twist_msg.linear.x && !std::isinf(dist)) {
-        ROS_INFO ("Valid Twist Message. No obstacle detected by laser.");
-        twist_pub.publish(twist_msg);
-        is_poss_msg.data = true;
+      float z = twist_msg.angular.z;
+      float rad_index = 0;
+      if (z < laser_msg.range_min && z > laser_msg.range_max) {
+        ROS_INFO ("Range out of bound");
+        is_poss_msg.data = false;
         safe_pub.publish(is_poss_msg);
       }
       else {
-        ROS_INFO ("Obstacle detected. Can't proceed");
-        is_poss_msg.data = false;
-        safe_pub.publish(is_poss_msg);
+        rad_index = std::abs(laser_msg.angle_min - z)/laser_msg.angle_increment;
+
+        float dist = laser_msg.ranges[rad_index];
+        ROS_INFO("Distance from laser is %f", dist);
+
+        if (dist > twist_msg.linear.x && !std::isinf(dist)) {
+          ROS_INFO ("Valid Twist Message. No obstacle detected by laser.");
+          twist_pub.publish(twist_msg);
+          is_poss_msg.data = true;
+          safe_pub.publish(is_poss_msg);
         }
+        else {
+          ROS_INFO ("Obstacle detected. Can't proceed");
+          is_poss_msg.data = false;
+          safe_pub.publish(is_poss_msg);
+        }
+      }
     }
 
     return 0;
