0976749589c45f6bd63e4eca7b244cb765a2997b
fri <jacqueline.gibson@utexas.edu>
2018-03-25 14:59:47 -0500

    initial spiral framework

--- bwi_wandering/src/safety.cpp
+++ bwi_wandering/src/safety.cpp
@@ -6,21 +6,55 @@
 #include <cmath>
 
 sensor_msgs::LaserScan laser_msg;
+geometry_msgs::Twist meas_mes;
+
+ros::Publisher safe_pub;
+ros::Subscriber meas_sub;
+ros::Subscriber laser_sub;
 
 void laser_cb(const sensor_msgs::LaserScan::ConstPtr& msg) {
   ROS_INFO("Laser Data Received\n");
   laser_msg = *msg;
 }
 
+//Callback that checks if the move direction is valid
+void meas_cb(const geometry_msgs::Twist::ConstPtr& msg) {
+  meas_mes = *msg;
+  std_msgs::Bool is_poss_msg;
+  float z = meas_mes.angular.z;
+  float rad_index = 0;
+  if (z < laser_msg.range_min && z > laser_msg.range_max) {
+    ROS_INFO ("Range out of bound");
+    is_poss_msg.data = false;
+    safe_pub.publish(is_poss_msg);
+  }
+  else {
+    rad_index = std::abs(laser_msg.angle_min - z)/laser_msg.angle_increment;
+
+    float dist = laser_msg.ranges[rad_index];
+    ROS_INFO("Distance from laser is %f", dist);
+
+    if (dist > meas_mes.linear.x && !std::isinf(dist)) {
+      ROS_INFO ("Valid Twist Message. No obstacle detected by laser.");
+      is_poss_msg.data = true;
+      safe_pub.publish(is_poss_msg);
+    }
+    else {
+      ROS_INFO ("Obstacle detected. Can't proceed");
+      is_poss_msg.data = false;
+      safe_pub.publish(is_poss_msg);
+    }
+  }
+}
 
 int main(int argc, char **argv) {
     ROS_INFO("Started");
     ros::init(argc, argv, "safety");
     ros::NodeHandle nh_;
 
-    ros::Subscriber laser_sub = nh_.subscribe("/scan_filtered", 1000, laser_cb);
-    ros::Publisher twist_pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
-    ros::Publisher safe_pub = nh_.advertise<std_msgs::Bool>("/cmd_safe",1000);    
+    laser_sub = nh_.subscribe("/scan_filtered", 1000, laser_cb);
+    meas_sub = nh_.subscribe("/safety_meas", 1000, meas_cb);
+    safe_pub = nh_.advertise<std_msgs::Bool>("/cmd_safe",1000);
 
     ros::Time start_time = ros::Time::now();
     ros::Duration timeout(0.5);
@@ -28,42 +62,8 @@ int main(int argc, char **argv) {
     while(ros::Time::now()-start_time < timeout)
       ros::spinOnce();
 
-    geometry_msgs::Twist twist_msg; 
-    std_msgs::Bool is_poss_msg;
-    // arbitrary value for now
-    twist_msg.linear.x = 0.50;
-    twist_msg.angular.z = 0;
-
     // assuming that this value is not arbritary, checking if it is okay to proceed
-    while(ros::ok()){
-      ros::spinOnce();
-
-      float z = twist_msg.angular.z;
-      float rad_index = 0;
-      if (z < laser_msg.range_min && z > laser_msg.range_max) {
-        ROS_INFO ("Range out of bound");
-        is_poss_msg.data = false;
-        safe_pub.publish(is_poss_msg);
-      }
-      else {
-        rad_index = std::abs(laser_msg.angle_min - z)/laser_msg.angle_increment;
-
-        float dist = laser_msg.ranges[rad_index];
-        ROS_INFO("Distance from laser is %f", dist);
-
-        if (dist > twist_msg.linear.x && !std::isinf(dist)) {
-          ROS_INFO ("Valid Twist Message. No obstacle detected by laser.");
-          twist_pub.publish(twist_msg);
-          is_poss_msg.data = true;
-          safe_pub.publish(is_poss_msg);
-        }
-        else {
-          ROS_INFO ("Obstacle detected. Can't proceed");
-          is_poss_msg.data = false;
-          safe_pub.publish(is_poss_msg);
-        }
-      }
-    }
+    while(ros::ok())
 
     return 0;
 }
