1a1a9246cb025d70fa9af8f4c9d23e0e0dba8a13
JGOOSH <jgoo@utexas.edu>
2018-03-25 21:35:04 -0500

    made safety.cpp and move.cpp to communicate

--- bwi_wandering/src/move.cpp
+++ bwi_wandering/src/move.cpp
@@ -1,11 +1,31 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "std_msgs/Bool.h"
+
+
+bool proceed;
+ros::Time proceed_time;
+ros::Time temp_time;
+
+void safe_cb(const std_msgs::Bool::ConstPtr& msg) {
+  proceed = msg->data;
+  proceed_time = ros::Time::now(); 
+}
+
 
 int main(int argc, char **argv) {
     ROS_INFO("Started");
     ros::init(argc, argv, "move");
     ros::NodeHandle nh_;
+
     ros::Publisher twist_pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1000);
+    ros::Publisher temp_pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_temp", 1000);
+    ros::Subscriber safe_sub = nh_.subscribe("/cmd_safe", 1000, safe_cb);
+
+    proceed = false;
+    proceed_time = ros::Time::now();
+    temp_time = ros::Time::now();
 
     ros::Time start_time = ros::Time::now();
     ros::Duration timeout(0.5);
@@ -13,22 +33,30 @@ int main(int argc, char **argv) {
     while(ros::Time::now()-start_time < timeout)
       ros::spinOnce();
 
-
     float distance = 1;
     float inc_val = .5;
 
 
     geometry_msgs::Twist twist_msg; 
+    geometry_msgs::TwistStamped temp_msg;
     twist_msg.linear.x = inc_val;
     twist_msg.angular.z = 0;
 
-    twist_pub.publish(twist_msg);
-    ros::Duration(2).sleep();
+    twist_pub.publish(temp_msg);
 
-    twist_msg.angular.z = 3.14/4;
+    ros::Duration timeout2(2.0);
+    ROS_INFO("Spinning for 0.5 seconds");
+    while(ros::Time::now()-start_time < timeout2)
+      ros::spinOnce();
 
-    twist_pub.publish(twist_msg);
-    ros::Duration(2).sleep();
+    if(proceed && proceed_time > temp_time)
+      twist_pub.publish(twist_msg);
+
+
+    // twist_msg.angular.z = 3.14/4;
+
+    // twist_pub.publish(twist_msg);
+    // ros::Duration(2).sleep();
 
     ROS_INFO("DONE WITH MOVEMENT");
     return 0;
--- bwi_wandering/src/safety.cpp
+++ bwi_wandering/src/safety.cpp
@@ -6,21 +6,16 @@
 #include <cmath>
 
 sensor_msgs::LaserScan laser_msg;
-geometry_msgs::Twist meas_mes;
-geometry_msgs::Twist vel_msg;
-
 ros::Publisher safe_pub;
-ros::Subscriber laser_sub;
-ros::Subscriber twist_sub;
 
 void laser_cb(const sensor_msgs::LaserScan::ConstPtr& msg) {
-  ROS_INFO("Laser Data Received\n");
+  // ROS_INFO("Laser Data Received\n");
   laser_msg = *msg;
 }
 
 //Callback that checks if the move direction is valid
 void vel_cb(const geometry_msgs::Twist::ConstPtr& msg) {
-  vel_msg = *msg;
+  geometry_msgs::Twist vel_msg = *msg;
   std_msgs::Bool is_poss_msg;
   float z = vel_msg.angular.z;
   float rad_index = 0;
@@ -32,18 +27,23 @@ void vel_cb(const geometry_msgs::Twist::ConstPtr& msg) {
   else {
     rad_index = std::abs(laser_msg.angle_min - z)/laser_msg.angle_increment;
 
-    float dist = laser_msg.ranges[rad_index];
-    ROS_INFO("Distance from laser is %f", dist);
+    int index_min = rad_index-5 >= 0? rad_index-5 : rad_index;
+    int index_max = rad_index+5 < laser_msg.ranges.size()? rad_index+5 : rad_index;
 
-    if (dist > vel_msg.linear.x && !std::isinf(dist)) {
+    is_poss_msg.data = true;
+
+    for (int i = index_min; i <= index_max; i++) {
+      float dist = laser_msg.ranges[rad_index];
+      ROS_INFO("Distance from laser is %f", dist);
+      if (!(dist > vel_msg.linear.x && !std::isinf(dist)))
+        is_poss_msg.data = false;
+    }
+
+    if (is_poss_msg.data) 
       ROS_INFO ("Valid Twist Message. No obstacle detected by laser.");
-      is_poss_msg.data = true;
-
-    }
-    else {
+    else 
       ROS_INFO ("Obstacle detected. Can't proceed");
-      is_poss_msg.data = false;
-    }
+
     safe_pub.publish(is_poss_msg);
   }
 }
@@ -53,18 +53,11 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "safety");
     ros::NodeHandle nh_;
 
-    twist_sub = nh_.subscribe("/cmd_vel", 1000, vel_cb);
-    laser_sub = nh_.subscribe("/scan_filtered", 1000, laser_cb);
+    ros::Subscriber twist_sub = nh_.subscribe("/cmd_vel_temp", 1000, vel_cb);
+    ros::Subscriber laser_sub = nh_.subscribe("/scan_filtered", 1000, laser_cb);
     safe_pub = nh_.advertise<std_msgs::Bool>("/cmd_safe",1000);
 
-    ros::Time start_time = ros::Time::now();
-    ros::Duration timeout(0.5);
-    ROS_INFO("Spinning for 0.5 seconds");
-    while(ros::Time::now()-start_time < timeout)
-      ros::spinOnce();
-
-    // assuming that this value is not arbritary, checking if it is okay to proceed
-    while(ros::ok())
+    ros::spin();
 
     return 0;
 }
