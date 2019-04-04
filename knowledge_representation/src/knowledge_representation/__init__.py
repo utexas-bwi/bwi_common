a3e46b054c75597813a4c444132922069888f54d
Yuqian Jiang <jiangyuqian@utexas.edu>
2019-04-04 14:03:32 -0500

    fix get_default_ltmc in python

--- knowledge_representation/src/knowledge_representation/__init__.py
+++ knowledge_representation/src/knowledge_representation/__init__.py
@@ -2,4 +2,4 @@ from knowledge_representation._libknowledge_rep_wrapper_cpp import LongTermMemor
 from knowledge_representation.memory_conduit import MemoryConduit
 
 def get_default_ltmc():
-    return LongTermMemoryConduit("127.0.0.1", 33060, "root", "", "knowledge_base")
+    return LongTermMemoryConduit("knowledge_base")
