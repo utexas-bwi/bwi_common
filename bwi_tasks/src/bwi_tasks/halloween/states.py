a2a4432f6926daa27c03e277175f3c676cb5361c
Yuqian Jiang <jiangyuqian@utexas.edu>
2019-10-29 17:04:43 -0500

    Update states to play from a list of spiderbot phrases
    Tune threshold to not scare people too much

--- bwi_tasks/src/bwi_tasks/halloween/states.py
+++ bwi_tasks/src/bwi_tasks/halloween/states.py
@@ -1,15 +1,18 @@
 import rospy
 import os
+import random
 from smach import State
 from smach_ros import SimpleActionState
 
 class PlaySound(State):
-    def __init__(self, audio_file):
+    def __init__(self, audio_dir, file_names):
         State.__init__(self, outcomes=["succeeded", "aborted"])
-        self.audio_file = audio_file
+        self.audio_dir = audio_dir
+	self.file_names = file_names
 
     def execute(self, userdata):
-        print self.audio_file
-        os.system("cvlc --play-and-exit " + self.audio_file)
+	audio_file = self.audio_dir + random.choice(self.file_names)
+        print audio_file
+        os.system("cvlc --play-and-exit " + audio_file)
 
         return "succeeded"
