433849d7393fadbcab98fcffcb08014d105ab3ca
Jin Soo Park <js.park@utexas.edu>
2020-02-17 23:33:37 -0600

    Add files via upload

--- multi_robot_collision_avoidance/src/multi_robot_collision_avoidance/MapHack.py
+++ multi_robot_collision_avoidance/src/multi_robot_collision_avoidance/MapHack.py
@@ -1,13 +1,18 @@
 import os
 
+import yaml
+from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage import feature
+
+# Ros related packages
+import rospkg
 
 class MapHack:
-    """MapHack(dir of map, dir of meta data(.yaml))
+    """MapHack()
 
-    Create a new Maphack object with given map file and meta data file.
+    Create a new Maphack object.
+    It automatically reads map, meta and segmentation data from utexas_gdc ros package.
+
     This object will extract 4 distance features of input point from map.
     Use visualization functions to see how it works.
 
@@ -15,142 +20,150 @@ class MapHack:
     d2 = distance from bold robot to point
     d3 = distance from left wall to point
     d4 = distance from right wall to point
+    ================================
+    Input
+    simulation: True if running on simulation, False otherwise.
     """
-    def __init__(self, map_path, metadata_path):
-        self.map_path = map_path
-        self.metadata_path = metadata_path
-        self.mapImage = self.__loadMapData(map_path)
-        self.meta = self.__loadMetaData(metadata_path)
-        self.mapEdge = self.__buildEdgeMap(self.mapImage)
+    def __init__(self, simulation):
+        mapDir =  {True: "maps/simulation/3ne/3ne.pgm",
+                   False:"maps/real/3/3.pgm"}
+        metaDir = {True: "maps/simulation/3ne/3ne.yaml",
+                   False:"maps/real/3/3.yaml"}
+        segDir =  {True: "maps/simulation/3ne/locations.pgm",
+                   False:"maps/real/3/locations.pgm"}
+        # Read nessesary information from utexas_gdc package.
+        rospack = rospkg.RosPack()
+        pkgPath = rospack.get_path('utexas_gdc')
+        mapFile = os.path.join(pkgPath, mapDir[simulation])
+        segFile = os.path.join(pkgPath, segDir[simulation])
+        metaFile = os.path.join(pkgPath, metaDir[simulation])
 
-    def __loadMapData(self, map_path):
-        mapImage = plt.imread(map_path)
-        self.h_, self.w_ = mapImage.shape
-        return mapImage
+        with open(metaFile, 'r') as file:
+            self.metaData = yaml.safe_load(file)
+        self.mapData = np.array(   Image.open(mapFile) )
+        self.h, self.w = self.mapData.shape
+        self.segData = np.asarray( Image.open(segFile) )
 
-    def __loadMetaData(self, metadata_path):
-        meta = {}
-        with open(metadata_path) as f:
-            for line in f:
-                key, val = line.strip().split(": ")
-                try:
-                    if(val[0]=="["): # if data is array
-                        meta[key] = np.fromstring(val[1:-1], dtype=float, sep=", ")
-                    else:
-                        meta[key] = float(val)
-                except:
-                    meta[key] = val
-        if(meta['image'] != self.map_path.split("/")[-1]):
-            print("meta data does not match to map data.")
-            print("{} != {}".format(meta['image'], self.map_path.split("/")[-1]))
-        return meta
+        self.filter = self.generate_filter([6,18,21,26,29]) # filter for simulation env
 
-    def __buildEdgeMap(self, image, sigma = 0.1):
-        # To measure the distance to the wall, the interior points are not needed.
-        # Edge map will reduce the search space while maintaining purformance
-        mapEdge = feature.canny(self.mapImage, sigma=sigma)
+    def pose2pix(self, pose):
+        """Convert real pose (x,y) [m] -> pixel pose [y,x]
+        ================================
+        Input
+        pose: (x,y) [m]
+        ================================
+        Return
+        [y,x] [pixel]
+        """
+        coord = np.array(pose)
+        pix = (coord - self.metaData['origin'][:-1]) / self.metaData['resolution']
+        pix = [self.h, 0] + pix[::-1] * [-1,1]
+        return pix
+    def pix2pose(self, pix):
+        """Convert pixel pose [y,x] -> real pose (x,y) [m]
+        ================================
+        Input
+        pose: [y,x] [pixel]
+        ================================
+        Return
+        (x,y) [m]
+        """
+        coord = ([self.h, 0] + np.array(pix) * [-1, 1])[::-1] # now [x,y]
+        pose = coord * self.metaData['resolution'] + self.metaData['origin'][:-1]
 
-        strictEdge = mapEdge * ~self.mapImage
-        return strictEdge
+        return pose
+    def sampling(self, sample_centre, window, n_sample):
+        """This function sample uniform distribution on a given rectangular area.
+        ================================
+        INPUT
+        sample_centre: origin of sampling area (y,x) [pixel]
+        window: size of sampling window [lower_left,upper_right], 2x2 # change to single window after integrate chicken to siren.
+        n_sample: number of sampling
+        ================================
+        Return
+        Samples (y's,x's)"""
+        bound = np.stack([sample_centre, sample_centre])
+        bound = bound + window
+        bound = np.clip(bound, [0,0], [self.h, self.w])
+        samples = np.random.uniform(bound[0], bound[1], (n_sample,2))
 
-    def showMap(self, mode='original'):
-        plt.figure(figsize=(32,12))
-        if(mode == 'original'):
-            plt.imshow(self.mapImage, cmap='gray')
-        elif(mode == 'edge'):
-            plt.imshow(self.mapEdge, cmap='gray')
-        else:
-            print("Available mode: [original, edge]")
-        return
+        return samples
 
-    def showMetaData(self):
-        print("Meta data of current map:")
-        for key in self.meta:
-            print("{}: {}".format(key, self.meta[key]))
-        return
-    def __dist(self, v1, v2=np.zeros(2)):
-        if(v1.size==2):
-            return np.linalg.norm(v1-v2)
+    def generate_filter(self, index):
+        """Generate filter that exclude all the non-hallway sample points
+        by comparing it with segmentation image
+        =============================
+        Input
+        index : the index of hallway; color [numpy array]"""
+        def sample_filter(samples):
+            qs = samples.astype(int)
+            idx = np.isin(self.segData[qs[:,0], qs[:,1]], index)
+            return samples[idx]
+        return sample_filter
+
+    def _dists(self, v1, v2):
+        v1 = np.array(v1).reshape(-1,2)
+        v2 = np.array(v2).reshape(-1,2)
         return np.linalg.norm(v1-v2, axis=1)
 
-    # Change pixel position to meter position
-    def pos2pixConverter(self, pos):
-        pix = (pos - self.meta['origin'][:-1])/self.meta['resolution']
-        if(pix.size==2):
-            pix[1] = self.h_ - pix[1]
-        else:
-            pix[:,1] = self.h_ - pix[:,1]
-        return pix.astype(int)
+    def extractFeature(self, coward, bold, wps, search_window = 1.6):
+        """Extract features from given waypoint position.
+        d1 := d( coward robot -> waypoint )
+        d2 := d( bold robot -> waypoint )
+        d3 := d( waypoint -> closest wall pt)
+        d4 := d( waypoint -> far wall pt)
+        ===============
+        INPUT
+        coward : pixelwise position of coward robot
+        bold   : pixelwise position of bold   robot
+        wp     : pixelwise position of waypoints
+        search_window : size of wall searching window
+        ===============
+        OUTPUT
+        features of all waypoints. [# waypoints, 4]"""
+        window = search_window/self.metaData['resolution']
+        features = np.empty((len(wps), 4))
 
-    def pix2posConverter(self, pix):
-        if(pix.size==2):
-            pix[1] =  self.h_ - pix[1]
-        else:
-            pix[:,1] = self.h_ - pix[:,1]
-        pos = pix * self.meta['resolution'] + self.meta['origin'][:-1]
-        return pos
+        features[:,0] = self._dists(coward, wps)
+        features[:,1] = self._dists(bold, wps)
 
-    def extractFeatures(self, chickenRobotPos, boldRobotPos, waypointPos,
-                        search_window = 1.5, mode = 'pos', verbose=False):
-        extractedFeatures = np.zeros(4) - 1 # For debuging purpose. -1 = error.
-        wall_disp = np.zeros((2,2)) # displacement of the wall from waypoint coordinate
+        for i, wp in enumerate(wps):
+            # Define quantized search area. area := sampling area(=0) + window
+            lower_bound = np.trunc(wp - window)
+            upper_bound = np.ceil (wp + window)
+            # clip search area within the map image.
+            lower_bound = np.clip(lower_bound, [0, 0], [self.h, self.w]).astype(int)
+            upper_bound = np.clip(upper_bound, [0, 0], [self.h, self.w]).astype(int)
+            # Find wall pts in the search region
+            # disp := displacement
+            disp = np.argwhere(self.mapData[lower_bound[0]:upper_bound[0],
+                                            lower_bound[1]:upper_bound[1]] ==0 )
+            disp = (disp + lower_bound) - wp
+            dist = self._dists(disp, [0,0])
+            if(len(dist) == 0):
+                features[i,2] = features[i,3] = window
+                continue
+            first_wall_pt_idx = np.argmin(dist)
+            features[i,2] = dist[first_wall_pt_idx]
 
-        window_pix = search_window / self.meta['resolution']
-        if(mode == 'pix'):
-            px, py = waypointPos
-            x, y = self.pix2posConverter(waypointPos)
-        elif(mode == 'pos'):
-            x,y = waypointPos
-            px, py = self.pos2pixConverter(waypointPos)
-        else:
-            print("mode = ['pix', 'pos']")
+            direction = (np.cross(disp[first_wall_pt_idx], bold-coward) > 0)
 
-        bold_pix = self.pos2pixConverter(boldRobotPos)
-        chicken_pix = self.pos2pixConverter(chickenRobotPos)
-        extractedFeatures[:2] = self.__dist([chicken_pix, bold_pix]-np.array([px,py]))
+            Rwps = wp - lower_bound
+            closest_pt = disp[first_wall_pt_idx] + Rwps
 
-        # clip search region
-        lowerY = int(max(py-window_pix, 0))
-        upperY = int(min(py+window_pix+1, self.mapEdge.shape[0]))
-        lowerX = int(max(px-window_pix, 0))
-        upperX = int(min(px+window_pix+1, self.mapEdge.shape[1]))
-        search_area = self.mapEdge[lowerY:upperY, lowerX:upperX]
-        # convert index to displacement
-        delta = np.array([min(py-lowerY,window_pix), min(px-lowerX,window_pix)])
-        displacement = np.argwhere(search_area!=0) - delta   # [y, x]
-        distance = self.__dist(displacement)
+            ind = (np.dot(disp, disp[first_wall_pt_idx])<-0.)
 
-        idx = np.argsort(distance)
-        if(idx.size == 0):
-            if(verbose): print("({},{}) is an open area. PIXEL LOCATION: ({},{})".format(x,y, px, py))
-            wall_disp[0] = wall_disp[1] = [window_pix,0]
-        else:
-            displacement = displacement[idx]
-            wall_disp[0] = displacement[0]
+            disp = disp[ind,:]
+            dist = dist[ind]
 
-            moveDirection = (np.array(bold_pix) - np.array(chicken_pix))[::-1]
-            baseline = np.cross(moveDirection, wall_disp[0])
-            indicator = np.cross(moveDirection, displacement) * baseline
-            mask = (indicator<0)
-            if(np.sum(mask)==0):
-                if(verbose): print("({},{}) is an semi-open area. PIXEL LOCATION: ({},{})".format(x,y, px, py))
-                wall_disp[1] = [window_pix,0]
+            if(np.sum(ind) == 0):
+                features[i,3] = 32
             else:
-                displacement = displacement[mask]
-                wall_disp[1] = displacement[0]
-            if(baseline<0):
-                wall_disp = wall_disp[::-1]
+                idx = np.argmin(dist)
+                features[i,3] = dist[idx]
 
-        # DEBUG
-        if(verbose):
-            wall_idx = np.argwhere(search_area!=0)
-            plt.imshow(search_area, cmap='gray') # show search area
-            plt.scatter(px-lowerX,py-lowerY) # show waypoint
-            plt.scatter(wall_idx.T[1], wall_idx.T[0]) # Show wall points
-            plt.scatter(displacement[:,1]+delta[1], displacement[:,0]+delta[0])
-            plt.scatter(wall_disp[0][1]+delta[1], wall_disp[0][0]+delta[0], color='r') # Show closest wall
-            plt.scatter(wall_disp[1][1]+delta[1], wall_disp[1][0]+delta[0], color='r')
-            plt.savefig("HowModuleWorks.png")
-
-        extractedFeatures[2:] = self.__dist(wall_disp)
-        return extractedFeatures * self.meta['resolution']
+            if(direction == True):
+                tmp = features[i,2]
+                features[i,2] = features[i,3]
+                features[i,3] = tmp
+        return features * self.metaData["resolution"]
