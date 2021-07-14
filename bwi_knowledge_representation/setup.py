60a6b600f76df2e1a9fae98c90951a66eb544de4
YuqianJiang <jiangyuqian@utexas.edu>
2021-07-14 11:50:54 -0700

    Update setup.py

--- bwi_knowledge_representation/setup.py
+++ bwi_knowledge_representation/setup.py
@@ -5,7 +5,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['bwi_knowledge_representation'],
-    package_dir={'': 'src'})
+    packages=['bwi_knowledge_representation'])
+    # packages=['bwi_knowledge_representation'],
+    # package_dir={'': 'src'})
 
 setup(**setup_args)
