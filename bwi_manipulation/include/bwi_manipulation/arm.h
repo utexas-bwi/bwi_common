2b0a49bcc75f4e790d4ff6b70d64792c954c343c
Corrie <corrievansice@gmail.com>
2022-07-20 16:02:53 -0500

    removed bwi_manipulation

--- bwi_manipulation/include/bwi_manipulation/arm.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-namespace bwi_manipulation {
-    class Arm {
-    //TODO: Figure out this abstraction
-        // This is a stub until we get a second arm and actually have to figure out a reasonable
-        // common interface.
-    };
-}
